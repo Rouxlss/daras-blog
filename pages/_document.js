d9779e91f12e98fbe7f583801fd835f741048c27
Rouxls <adolfo.flores@focusservices.com>
2022-05-02 01:58:32 -0600

    Changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const origin = (typeof window !== 'undefined') ? window.location.origin : ''
+
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initalProps = await Document.getInitialProps(ctx)
@@ -12,6 +14,9 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.1/css/all.css" />
+                    <meta property="og:title" content="Literatura y realidad" />
+                    <meta property="og:description" content="Literatura salvadoreña" />
+                    <meta property="og:image" content={`${origin}/img/banner.jpg`} />
                 </Head>
                 <body>
                     <Main />
